7e33930c61d5cd399c703efd7f308277a6544220
ola <olczali4@gmail.com>
2023-01-12 14:48:25 +0100

    z zajec

---
+++ cmake-build-debug/CMakeFiles/mspRefleks.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mspRefleks.dir/gra.c.o"
+  "CMakeFiles/mspRefleks.dir/gra.c.o.d"
+  "CMakeFiles/mspRefleks.dir/kafelek.c.o"
+  "CMakeFiles/mspRefleks.dir/kafelek.c.o.d"
+  "CMakeFiles/mspRefleks.dir/lcd.c.o"
+  "CMakeFiles/mspRefleks.dir/lcd.c.o.d"
+  "CMakeFiles/mspRefleks.dir/main.c.o"
+  "CMakeFiles/mspRefleks.dir/main.c.o.d"
+  "CMakeFiles/mspRefleks.dir/portyLcd.c.o"
+  "CMakeFiles/mspRefleks.dir/portyLcd.c.o.d"
+  "CMakeFiles/mspRefleks.dir/portyUart.c.o"
+  "CMakeFiles/mspRefleks.dir/portyUart.c.o.d"
+  "CMakeFiles/mspRefleks.dir/uart.c.o"
+  "CMakeFiles/mspRefleks.dir/uart.c.o.d"
+  "CMakeFiles/mspRefleks.dir/uartservice.c.o"
+  "CMakeFiles/mspRefleks.dir/uartservice.c.o.d"
+  "CMakeFiles/mspRefleks.dir/znaki.c.o"
+  "CMakeFiles/mspRefleks.dir/znaki.c.o.d"
+  "mspRefleks"
+  "mspRefleks.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mspRefleks.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- uartservice.c
+++ uartservice.c
@@ -2,12 +2,13 @@
 #include "portyLcd.h"
 #include "lcd.h"
 #include <stdio.h>
-#include <string.h>
+#include<stdlib.h>
 #include <stdbool.h>
 #include "uart.h"
+#include <string.h>
 
 int numberOfScores = 0;
-int currentLetter = 'A';     // wartosc pierwszego znaku w tablicy ASCII
+int currentLetter = 'A'; // wartosc pierwszego znaku w tablicy ASCII
 // Struktura przechowujqca wyniki z gry
 
 struct Score {
@@ -15,41 +16,55 @@ struct Score {
     int points;
 };
 
-
 char readChar() {
     currentLetter = 65;
-    bool button1 = !(P4IN & BIT4);      // zdefiniowanie boola okreslajacego klikniecie przycisku pierwszego
-    bool button2 = !(P4IN & BIT5);      // zdefiniowanie boola okreslajacego klikniecie przycisku drugiego
-    bool button3 = !(P4IN & BIT6);      // zdefiniowanie boola okreslajacego klikniecie przycisku trzeciego
+    bool button1 = !(P4IN & BIT4); // zdefiniowanie boola okreslajacego klikniecie przycisku pierwszego
+    bool button2 = !(P4IN & BIT5); // zdefiniowanie boola okreslajacego klikniecie przycisku drugiego
+    bool button3 = !(P4IN & BIT6); // zdefiniowanie boola okreslajacego klikniecie przycisku trzeciego
 
     if (button1) {
-        Delayx100us(200);                           // opoznienie
-        clearDisplay();                             // wyczyszczenie wyswietlacza
+        Delayx100us(200); // opoznienie
+        clearDisplay(); // wyczyszczenie wyswietlacza
 
-        if (currentLetter > 65) {                    // sprawdzenie czy obecna wartosc znaku ASCII nie będzie ponizej 'A'
-            SEND_CHAR(--currentLetter);              // po jego dekrementacji
+        if (currentLetter > 65) { // sprawdzenie czy obecna wartosc znaku ASCII nie będzie ponizej 'A'
+            SEND_CHAR(--currentLetter); // po jego dekrementacji
         }
     }
 
     if (button2) {
-        Delayx100us(200);                           // opoznienie
-        UartCharTransmit(currentLetter);             // wyslanie obecnej litery do terminmala
+        Delayx100us(200); // opoznienie
         return currentLetter;
     }
 
     if (button3) {
-        Delayx100us(200);                           // opoznienie
-        clearDisplay();                             // wyczyszczenie wyswietlacza
-        if (currentLetter < 90) {                    // sprawdzenie czy obecna wartosc znaku ASCII nie będzie powyzej 'Z'
-            SEND_CHAR(++currentLetter);              // po jego inkrementacji
+        Delayx100us(200); // opoznienie
+        clearDisplay(); // wyczyszczenie wyswietlacza
+        if (currentLetter < 90) { // sprawdzenie czy obecna wartosc znaku ASCII nie będzie powyzej 'Z'
+            SEND_CHAR(++currentLetter); // po jego inkrementacji
         }
+    }
+}
+//metoda wysylajaca tablice charow na terminal
+//wywala jakis blad
+void sendCharsToTerminal(char* c[]){
+    for(int i = 0; i < sizeof(c); i++){
+        UartCharTransmit(*c[i]); //wyslanie chara na terminal
+    }
+}
+
+//metoda wysylajaca tablice charow na plytke LCD
+void sendCharsToLCD(char* c[]){
+    for(int i = 0; i < sizeof(c); i++){
+        SEND_CHAR(*c[i]); //wyslanie chara na LCD
     }
 }
 
 char *podajInicjaly(){
     char name[2];
-    name[0] = readChar();
-    name[1] = readChar();
+    name[0] = readChar(); //uzytkownik wybiera pierwsza litere swojego inicjalu
+    name[1] = readChar(); //uzytkownik wybiera druga litere swojego inicjalu
+    char* send[32]= "utworzono inicjaly";
+    sendCharsToTerminal(send);
     return name;
 }
 
@@ -57,32 +72,27 @@ char *podajInicjaly(){
 struct Score scores[10];
 
 int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;   // Disable watchdog timer
-    P4DIR &= ~BIT4;             // zainicjalizowanie pierwszego przycisku
-    P4DIR &= ~BIT5;             // zainicjalizowanie drugiego przycisku
-    P4DIR &= ~BIT6;             // zainicjalizowanie trzeciego przycisku
-    P4DIR &= ~BIT7;             // zainicjalizowanie czwartego przycisku
-    WDTCTL = WDTPW + WDTHOLD;   // wylaczenie WDT
+    WDTCTL = WDTPW | WDTHOLD; // Disable watchdog timer
+    P4DIR &= ~BIT4; // zainicjalizowanie pierwszego przycisku
+    P4DIR &= ~BIT5; // zainicjalizowanie drugiego przycisku
+    P4DIR &= ~BIT6; // zainicjalizowanie trzeciego przycisku
+    P4DIR &= ~BIT7; // zainicjalizowanie czwartego przycisku
+    WDTCTL = WDTPW + WDTHOLD; // wylaczenie WDT
 
-    InitPortsLcd();             // inicjalizacja portow LCD
-    InitLCD();                  // inicjalizacja LCD
-    clearDisplay();             // czyszczenie wyswietlacza
-    InitUart(1200);             // inicjalizacja UARTa predkosci transmisji 2400 b/s
+    InitPortsLcd(); // inicjalizacja portow LCD
+    InitLCD(); // inicjalizacja LCD
+    clearDisplay(); // czyszczenie wyswietlacza
+    InitUart(1200); // inicjalizacja UARTa predkosci transmisji 2400 b/s
 
-    _EINT();                    // wylaczenie przerwan
-
+    _EINT(); // wylaczenie przerwan
 }
 
-void sendCharsToTerminal(char c[]){
-    for(int i = 0; i < sizeof(c); i++){
-        SEND_CHAR(c[i]);
-    }
-}
 
+//metoda sortujaca tablice wynikow
 void sortScores(struct Score scores[], int n) {
     int i, j;
     for (i = 0; i < n - 1; i++) {
-        // Szukanie najwi?kszego elementu w tablicy
+// Szukanie najwi?kszego elementu w tablicy
         int max_idx = i;
         for (j = i + 1; j < n; j++) {
             if (scores[j].points > scores[max_idx].points) {
@@ -90,13 +100,14 @@ void sortScores(struct Score scores[], int n) {
             }
         }
 
-        // Zamiana miejscami najwi?kszego elementu z elementem na pozycji i
+// Zamiana miejscami najwi?kszego elementu z elementem na pozycji i
         struct Score temp = scores[i];
         scores[i] = scores[max_idx];
         scores[max_idx] = temp;
     }
 }
 
+//metoda tworzaca nowy rekord po rozegraniu gry
 struct Score createScore(char name[], int points) {
     struct Score newScore;
     strcpy(newScore.name, name);
@@ -104,6 +115,7 @@ struct Score createScore(char name[], int points) {
     return newScore;
 }
 
+//metoda dodajaca nowy rekord do tablicy wynikow
 void addScore(struct Score scores[], int arraySize, struct Score newScore) {
     if (numberOfScores <= 9) {
         scores[numberOfScores] = newScore;
@@ -114,50 +126,82 @@ void addScore(struct Score scores[], int arraySize, struct Score newScore) {
     }
     sortScores(scores, arraySize);
 }
+char * toArray(int number)
+{
+    int n = log10(number) + 1;
+    int i;
+    char *numberArray = calloc(n, sizeof(char));
+    for (i = n-1; i >= 0; --i, number /= 10)
+    {
+        numberArray[i] = (number % 10) + '0';
+    }
+    return numberArray;
+}
 
+//metoda wypisujaca tablice wynikow do terminala
 void printScores(struct Score scores[], int n) {
     int i;
-    char points_str[10];
+    char* points_str[10];
     for (i = 1; i <= n; i++) {
+//bledy
         sendCharsToTerminal(scores[i].name);
-        SEND_CHAR('.');
-        SEND_CHAR(' ');
-        sprintf(points_str,"%ld", scores[i].points);
+        UartCharTransmit('.');
+        UartCharTransmit(' ');
+//bledy
+        points_str = toArray(scores[i].points);
         sendCharsToTerminal(points_str);
     }
 }
 
+//metoda wyswietlajaca na LCD zwyciestwo lub przegrana
 void endOfTheGame(bool isWin, int result){
     if(isWin){
-        sendCharsToTerminal("Wygrales! ");
+        char* send[32] ="Wygrales! ";
+        sendCharsToLCD(send);
     } else {
-        sendCharsToTerminal("Przegrales! ");
+        char* send2[32] ="Przegrales! ";
+        sendCharsToLCD(send2);
     }
     printScores(scores,numberOfScores);
 }
-void menu() {
-    bool button1 = !(P4IN & BIT4);      // zdefiniowanie boola okreslajacego klikniecie przycisku pierwszego
-    bool button2 = !(P4IN & BIT5);      // zdefiniowanie boola okreslajacego klikniecie przycisku drugiego
-    bool button3 = !(P4IN & BIT6);      // zdefiniowanie boola okreslajacego klikniecie przycisku trzeciego
-    bool button4 = !(P4IN & BIT7);      // zdefiniowanie boola okreslajacego klikniecie przycisku czwartego
 
+void menu() {
+    bool button1 = !(P4IN & BIT4); // zdefiniowanie boola okreslajacego klikniecie przycisku pierwszego
+    bool button2 = !(P4IN & BIT5); // zdefiniowanie boola okreslajacego klikniecie przycisku drugiego
+    bool button3 = !(P4IN & BIT6); // zdefiniowanie boola okreslajacego klikniecie przycisku trzeciego
+    bool button4 = !(P4IN & BIT7); // zdefiniowanie boola okreslajacego klikniecie przycisku czwartego
+    char* nowaGra[32] = "1. Nowa gra.";
+    char* wyswietl[32] = "2. Wyswietl wyniki.";
+    char* zakoncz[32] = "3. Zakoncz gre.";
+    sendCharToTerminal(nowaGra);
+    sendCharToTerminal(wyswietl);
+    sendCharToTerminal(zakoncz);
 
     if (button1) {
-        Delayx100us(200);                           // opoznienie
-        clearDisplay();                             // wyczyszczenie wyswietlacza
-        // start new game
+        Delayx100us(200); // opoznienie
+        clearDisplay(); // wyczyszczenie wyswietlacza
+// start new game
+/*sendCharToTerminal("Podaj inicjaly");
+char inicjaly[2] = podajInicjaly();
+int wynik = 0;//wynik z skadstam
+struct Score sc = createScore(inicjaly, wynik);
+addScore(scores, numberOfScores, sc);
+clearDisplay();
+endOfTheGame(wygrales, wynik);*/
     }
 
     if (button2) {
-        Delayx100us(200);                           // opoznienie
-        clearDisplay();                             // wyczyszczenie wyswietlacza
-        //drukowanie wynikow
+        Delayx100us(200); // opoznienie
+        clearDisplay(); // wyczyszczenie wyswietlacza
+//drukowanie wynikow
         printScores(scores,numberOfScores);
     }
 
     if (button3) {
-        Delayx100us(200);                           // opoznienie
-        clearDisplay();                             // wyczyszczenie wyswietlacza
-        //wyjscie z gry
+        Delayx100us(200); // opoznienie
+        clearDisplay(); // wyczyszczenie wyswietlacza
+        char* send[32] = "Koniec gry!";
+        sendCharToTerminal(send);
+//wyjscie z gry
     }
 }
