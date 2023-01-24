6d339fb22e98d9e5528de635eae354562c068f99
papryku <supteros@gmail.com>
2023-01-24 12:14:41 +0100

    scalenie zmian w uartservice 24.01
    + czyszczenie niepotrzebnych rzeczy, formatowanie, poprawki pliku naglowkowego

--- uartservice.c
+++ uartservice.c
@@ -1,4 +1,4 @@
-#include <msp430x14x.h> 
+#include <msp430x14x.h>
 #include "portyLcd.h"
 #include "lcd.h"
 #include "uart.h"
@@ -10,23 +10,25 @@
 
 #ifndef PRZYCISKI
 #define PRZYCISKI
-#define PRZYCISK1 (BIT4&P4IN)
-#define PRZYCISK2 (BIT5&P4IN)
-#define PRZYCISK3 (BIT6&P4IN)
-#define PRZYCISK4 (BIT7&P4IN)
+#define PRZYCISK1 (BIT4 & P4IN)
+#define PRZYCISK2 (BIT5 & P4IN)
+#define PRZYCISK3 (BIT6 & P4IN)
+#define PRZYCISK4 (BIT7 & P4IN)
 #endif
 
-//jeśli potrzebne to będzie w operacjach poza biblioteką, to należy zdefiniować jedynie w pliku nagłówkowym i tu zostawić ustawienie wartości bez deklarowania jako int
+// jeśli potrzebne to będzie w operacjach poza biblioteką, to należy zdefiniować jedynie w pliku nagłówkowym i tu zostawić ustawienie wartości bez deklarowania jako int
 int numberOfScores = 0;
 int currentLetter = 'A'; // wartosc pierwszego znaku w tablicy ASCII
 int wynikAktualnej = 0;
 char inicjalyAktualnej[2];
 
-void zmienWynik(int num){
-  wynikAktualnej = wynikAktualnej + num;
-  if(wynikAktualnej < 0){
-  	wynikAktualnej = 0;
-  }
+void zmienWynik(int num)
+{
+    wynikAktualnej = wynikAktualnej + num;
+    if (wynikAktualnej < 0)
+    {
+        wynikAktualnej = 0;
+    }
 }
 
 char readChar()
@@ -34,7 +36,7 @@ char readChar()
     gotoSecondLine();
     currentLetter = 65;
     SEND_CHAR('A');
-    while(1)
+    while (1)
     {
 
         if (!(PRZYCISK1))
@@ -44,7 +46,7 @@ char readChar()
             {
                 clearDisplay();
                 SEND_CHARS("Podaj Inicjaly");
-                gotoSecondLine();// sprawdzenie czy obecna wartosc znaku ASCII nie będzie ponizej 'A'
+                gotoSecondLine();           // sprawdzenie czy obecna wartosc znaku ASCII nie będzie ponizej 'A'
                 SEND_CHAR(--currentLetter); // po jego dekrementacji
             }
         }
@@ -63,7 +65,7 @@ char readChar()
             {
                 clearDisplay();
                 SEND_CHARS("Podaj Inicjaly");
-                gotoSecondLine();// sprawdzenie czy obecna wartosc znaku ASCII nie będzie powyzej 'Z'
+                gotoSecondLine();           // sprawdzenie czy obecna wartosc znaku ASCII nie będzie powyzej 'Z'
                 SEND_CHAR(++currentLetter); // po jego inkrementacji
             }
         }
@@ -73,7 +75,7 @@ char readChar()
 char *podajInicjaly()
 {
     char name[2];
-    name[0] = readChar(); //uzytkownik wybiera pierwsza litere swojego inicjalu
+    name[0] = readChar(); // uzytkownik wybiera pierwsza litere swojego inicjalu
     SEND_CHARS("Podaj inicjaly:");
     DelayB(50);
     gotoSecondLine();
@@ -94,31 +96,13 @@ char *podajInicjaly()
 // Tablica wynikow
 struct Score scores[10];
 
-/*
-int main(void) {
-WDTCTL = WDTPW | WDTHOLD; // Disable watchdog timer
-P4DIR &= ~BIT4; // zainicjalizowanie pierwszego przycisku
-P4DIR &= ~BIT5; // zainicjalizowanie drugiego przycisku
-P4DIR &= ~BIT6; // zainicjalizowanie trzeciego przycisku
-P4DIR &= ~BIT7; // zainicjalizowanie czwartego przycisku
-WDTCTL = WDTPW + WDTHOLD; // wylaczenie WDT
-
-InitPortsLcd(); // inicjalizacja portow LCD
-InitLCD(); // inicjalizacja LCD
-clearDisplay(); // czyszczenie wyswietlacza
-InitUart(1200); // inicjalizacja UARTa predkosci transmisji 2400 b/s
-
-_EINT(); // wylaczenie przerwan
-}*/
-
-
-//metoda sortujaca tablice wynikow
+// metoda sortujaca tablice wynikow
 void sortScores(int n)
 {
     int i, j;
     for (i = 0; i < n - 1; i++)
     {
-// Szukanie najwi?kszego elementu w tablicy
+        // Szukanie najwi?kszego elementu w tablicy
         int max_idx = i;
         for (j = i + 1; j < n; j++)
         {
@@ -128,14 +112,14 @@ void sortScores(int n)
             }
         }
 
-// Zamiana miejscami najwi?kszego elementu z elementem na pozycji i
+        // Zamiana miejscami najwi?kszego elementu z elementem na pozycji i
         struct Score temp = scores[i];
         scores[i] = scores[max_idx];
         scores[max_idx] = temp;
     }
 }
 
-//metoda tworzaca nowy rekord po rozegraniu gry
+// metoda tworzaca nowy rekord po rozegraniu gry
 struct Score createScore(char name[], int points)
 {
     Score newScore;
@@ -144,7 +128,7 @@ struct Score createScore(char name[], int points)
     return newScore;
 }
 
-//metoda dodajaca nowy rekord do tablicy wynikow
+// metoda dodajaca nowy rekord do tablicy wynikow
 void addScore(struct Score newScore)
 {
     if (numberOfScores <= 9)
@@ -156,113 +140,105 @@ void addScore(struct Score newScore)
     {
         scores[9] = newScore;
     }
-// sortScores(scores, arraySize);
+    sortScores(numberOfScores);
 }
-//metoda wypisujaca tablice wynikow do terminala
+// metoda wypisujaca tablice wynikow do terminala
 void printScores()
 {
-    for (int i = 0; i <= numberOfScores-1; i++)
+    for (int i = 0; i < numberOfScores; i++)
     {
-//bledy
-      clearDisplay();
-        SEND_CHARS(scores[i].name);
-        SEND_CHAR(':');
-//bledy
-        SEND_NUMBER(scores[i].points);
-        gotoSecondLine();
-
-        /*SEND_CHARS(scores[i + 1].name);
-        SEND_CHAR(':');
-//bledy
-        char *points_str2 = intToCharArray(scores[i + 1].points);
-        SEND_CHARS(points_str2);
+        // bledy
         clearDisplay();
-        DelayB(10000);*/
+        SEND_NUMBER(i + 1);
+        SEND_CHARS(". miejsce");
+        gotoSecondLine();
+        SEND_CHAR(scores[i].name[0]);
+        SEND_CHAR(scores[i].name[1]);
+        SEND_CHARS(" : ");
+        // bledy
+        SEND_NUMBER(scores[i].points);
+        //    gotoSecondLine();
+        DelayB(500);
+        //     SEND_CHARS(scores[i + 1].name);
+        //    SEND_CHARS(" : ");
+        // bledy
+        //     SEND_NUMBER(scores[i+1].points);
     }
-}
-
-//metoda wyswietlajaca na LCD zwyciestwo lub przegrana\
-//deprecated
-void endOfTheGame(int isWin, int result) //nie wiem co z tym resultem
-{
-    if(isWin)
-    {
-        char send[32] ="Wygrales! ";
-        sendCharsToLCD(send);
-    }
-    else
-    {
-        char send2[32] ="Przegrales! ";
-        sendCharsToLCD(send2);
-    }
-    printScores(scores);
-}
+} // brakuje wychodzenia z tego i ogolnie wszystkiego co tam wam nie dziala
 
 void menu(int *rozpoczeta)
 {
+    SEND_CMD(CLR_DISP);
     SEND_CHARS("1.Nowa gra.");
     gotoSecondLine();
     SEND_CHARS("2.Wyswietl wyniki.");
-	DelayB(100);
-    while(1)
+    DelayB(100);
+    while (1)
     {
         if (!(PRZYCISK1))
         {
-            DelayB(100); // opoznienie
+            DelayB(100);    // opoznienie
             clearDisplay(); // wyczyszczenie wyswietlacza
-// start new game
+                            // start new game
             SEND_CHARS("Podaj inicjaly");
 
-    //w skrócie chcę mieć globalne inicjały i wynik w uartservice, które będą zmieniały się co rozpoczęcie gry
-    //potem w każdej potrzebnej funkcji będzie można wykorzystać te wartości zamiast się bawić we wskaźniki
+            // w skrócie chcę mieć globalne inicjały i wynik w uartservice, które będą zmieniały się co rozpoczęcie gry
+            // potem w każdej potrzebnej funkcji będzie można wykorzystać te wartości zamiast się bawić we wskaźniki
             char *inicjaly = podajInicjaly();
-            strcpy(inicjalyAktualnej, inicjaly); //oby to dzialalo
-       		SEND_CMD(CLR_DISP);
-            //rozpoczyna gre (gra.c)
+            strcpy(inicjalyAktualnej, inicjaly); // oby to dzialalo
+            SEND_CMD(CLR_DISP);
+            // rozpoczyna gre (gra.c)
             rozpocznijGre();
-			*rozpoczeta = 1;
+            *rozpoczeta = 1;
 
             return;
         }
 
         if (!(PRZYCISK2))
         {
-            DelayB(100); // opoznienie
+            DelayB(100);    // opoznienie
             clearDisplay(); // wyczyszczenie wyswietlacza
-//drukowanie wynikow
+                            // drukowanie wynikow
             printScores(scores);
             return;
         }
-
     }
 }
 
-
-//przerzucam swoje z gry.c zeby nie bawic sie w rzucanie wskaznikiem do inicjalow
-void koniecGry(int * rozpoczeta){
+// przerzucam swoje z gry.c zeby nie bawic sie w rzucanie wskaznikiem do inicjalow
+void koniecGry(int *rozpoczeta)
+{
     TACTL &= ~MC_1;
-  	*rozpoczeta = 0;
-    
+    *rozpoczeta = 0;
+
     SEND_CMD(CLR_DISP);
-    SET_CURSOR(4,1);
-    SEND_CHARS("PRZEGRALES");
-    SET_CURSOR(2,2);
-    SEND_CHAR(inicjalyAktualnej[0]); SEND_CHAR(inicjalyAktualnej[1]);
+    SET_CURSOR(4, 1);
+    SEND_CHARS("PRZEGRYWASZ");
+    if (wynikAktualnej == 0)
+        SEND_CHARS(" XDDD");
+    SET_CURSOR(2, 2);
+    SEND_CHAR(inicjalyAktualnej[0]);
+    SEND_CHAR(inicjalyAktualnej[1]);
     SEND_CHARS(", WYNIK:");
-	if(wynikAktualnej==0) { SEND_CHAR('0'); }
-    else { SEND_NUMBER(wynikAktualnej); }
-	
-	//struct Score sc = createScore(inicjalyAktualnej, wynikAktualnej);
-    //addScore(sc);
-	
-    DelayB(200);
-    //dopoki nie jest wcisniety
-    while(*rozpoczeta!=1){
-      if(!(PRZYCISK1)||!(PRZYCISK2)||!(PRZYCISK3)||!(PRZYCISK4)){
-        SEND_CMD(CLR_DISP);
-        //mam nadzieje, ze to juz zapewni tak naprawde mozliwosc ponownego zagrania
-        menu(rozpoczeta);
-      }
+    if (wynikAktualnej == 0)
+    {
+        SEND_CHARS('0 cielaku');
     }
-	//menu(rozpoczeta);
-} 
+    else
+    {
+        SEND_NUMBER(wynikAktualnej);
+    }
+
+    struct Score sc = createScore(inicjalyAktualnej, wynikAktualnej);
+    addScore(sc);
+
+    DelayB(200);
+    while (*rozpoczeta != 1)
+    {
+        if (!(PRZYCISK1) || !(PRZYCISK2) || !(PRZYCISK3) || !(PRZYCISK4))
+        {
+            SEND_CMD(CLR_DISP);
+            menu(rozpoczeta);
+        }
+    }
+}
--- uartservice.h
+++ uartservice.h
@@ -1,41 +1,45 @@
 
 // Struktura przechowujqca wyniki z gry
-typedef struct Score {
+typedef struct Score
+{
     char name[2];
     int points;
 } Score;
 
-//modyfikuje wynik aktualnej gry
+// modyfikuje wynik aktualnej gry
 void zmienWynik(int num);
 
+// funkcja pozwalająca wybrać literę, którą później zwraca
 char readChar();
-//metoda wysylajaca tablice charow na terminal
 
-//metoda wysylajaca tablice charow na plytke LCD
-void sendCharsToLCD(char c[]);
+// metoda wysylajaca tablice charow na plytke LCD
+// nie widze zeby byla w kodzie i/lub uzywana takze wyjebie jak jutro nie bedzie bledow
+// void sendCharsToLCD(char c[]);
 
+// funkcja pozwalająca wybrać inicjały, które później zwraca w tablicy
 char *podajInicjaly();
 
-//metoda sortujaca tablice wynikow
+// metoda sortujaca tablice wynikow
 void sortScores(int n);
 
-//metoda tworzaca nowy rekord po rozegraniu gry
+// metoda tworzaca nowy rekord po rozegraniu gry
 struct Score createScore(char name[], int points);
 
-//metoda dodajaca nowy rekord do tablicy wynikow
+// metoda dodajaca nowy rekord do tablicy wynikow
 void addScore(struct Score newScore);
 
-char* intToCharArray(int num);
+// funkcja zastąpiona SEND_NUMBER z lcd
+// char* intToCharArray(int num);
 
-//metoda wypisujaca tablice wynikow do terminala
+// metoda wypisujaca tablice wynikow do terminala
 void printScores();
 
-//metoda wyswietlajaca na LCD zwyciestwo lub przegrana
-//zmienie dzialanie troche
+// metoda wyswietlajaca na LCD zwyciestwo lub przegrana
+// zmienie dzialanie troche
 void endOfTheGame(int isWin, int result);
 
-//najwazniejsze co tu jest
-void menu(int * rozpoczeta);
+// najwazniejsze co tu jest tbh
+void menu(int *rozpoczeta);
 
-//przerzucam swoje z gry.c zeby nie bawic sie w rzucanie wskaznikiem do inicjalow
-void koniecGry(int * rozpoczeta); //char inicjaly[2], int wynik
+// metoda kończąca rozgrywkę, wyświetla końcowy wynik i inicjały, przekazuje je do tabeli wyników i powraca do menu()
+void koniecGry(int *rozpoczeta); // char inicjaly[2], int wynik
